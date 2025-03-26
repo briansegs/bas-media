cd96be46aeac4f16b633a4935008a2a9979f2570
bsegers <briansegs@gmail.com>
2025-03-26 15:35:18 -0400

    Fix nit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import clsx from "clsx";
+import React, { useState } from "react";
 
 const bool = true;
 
-const App = () => {
+const App: React.FC = () => {
+  const [toggleMsg, setToggleMsg] = useState(false);
+
   return (
     <div
       className={clsx(
-        "flex w-full justify-center", // Layout
+        "flex", // Layout
         "mt-8", // Spacing
-        "text-2xl text-amber-700 underline", // Typography & colors
-        bool && "w-full justify-center",
+        bool && "w-full flex-col items-center gap-8",
       )}
     >
-      Bas - Media
+      <button
+        className="cursor-pointer rounded-lg border-2 border-transparent px-4 py-2 text-2xl font-bold text-sky-400 hover:border-sky-300 hover:text-teal-500"
+        onClick={() => setToggleMsg((prev) => !prev)}
+      >
+        Bas - Media
+      </button>
+
+      {toggleMsg && (
+        <div className="bg-gradient-to-r from-teal-500 to-sky-500 bg-clip-text text-2xl text-transparent">
+          Welcome to my site!
+        </div>
+      )}
     </div>
   );
 };
