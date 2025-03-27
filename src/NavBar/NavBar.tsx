5bcff547089b3a43caeb1c4278c1ea11d46267d4
bsegers <briansegs@gmail.com>
2025-03-27 19:18:48 -0400

    Fix lint warnings

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import { NavLink } from "react-router";
 
 const NavBar: React.FC = () => {
-  const [theme, setTheme] = useState("light");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
