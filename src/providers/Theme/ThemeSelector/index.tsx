86e70aa972342adcf90c2919ae27c9fd8970d184
bsegers <briansegs@gmail.com>
2025-03-27 22:27:48 -0400

    Address nits

--- src/providers/Theme/ThemeSelector/index.tsx
+++ src/providers/Theme/ThemeSelector/index.tsx
@@ -4,11 +4,11 @@ import type { Theme } from "./types";
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "..";
-import { themeLocalStorageKey } from "./types";
+import { defaultTheme, themeLocalStorageKey } from "./types";
 
 const ThemeSelector: React.FC = () => {
   const { setTheme } = useTheme();
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState<Theme>(defaultTheme);
 
   const onThemeChange = (themeToSet: Theme) => {
     setTheme(themeToSet);
@@ -17,11 +17,13 @@ const ThemeSelector: React.FC = () => {
 
   React.useEffect(() => {
     const preference = window.localStorage.getItem(themeLocalStorageKey);
-    setValue(preference ?? "light");
+    setValue((preference as Theme) ?? defaultTheme);
   }, []);
 
   return (
     <button
+      aria-label={`Switch to ${value === "light" ? "dark" : "light"} mode`}
+      title={`Switch to ${value === "light" ? "dark" : "light"} mode`}
       className="cursor-pointer"
       onClick={() => {
         const nextTheme = value === "light" ? "dark" : "light";
