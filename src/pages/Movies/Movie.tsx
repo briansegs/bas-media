4829f8e6afbc41e0abbf7e4ae0510120c718959d
bsegers <briansegs@gmail.com>
2025-03-31 10:02:10 -0400

    refactor: Extract star rating component

--- src/pages/Movies/Movie.tsx
+++ src/pages/Movies/Movie.tsx
@@ -1,9 +1,6 @@
 import { Card } from "@/components/ui/card";
-import { Tooltip, TooltipTrigger } from "@/components/ui/tooltip";
-import { TooltipContent } from "@radix-ui/react-tooltip";
-import { ImStarFull } from "react-icons/im";
-
 import { Link } from "react-router";
+import StarRating from "./StarRating";
 
 const Movie = ({
   movie,
@@ -36,31 +33,7 @@ const Movie = ({
             {title}
           </h5>
 
-          <Tooltip>
-            <TooltipTrigger>
-              <div className="relative mt-2 h-fit overflow-hidden">
-                <div
-                  className="bg-background absolute top-0 left-0 size-full transition-transform"
-                  style={{
-                    transform: `translateX(${(vote_average / 10) * 100 || 0}%) `,
-                  }}
-                />
-
-                <div className="flex justify-center text-yellow-300">
-                  {[1, 2, 3, 4, 5].map((item) => (
-                    <ImStarFull key={item} />
-                  ))}
-                </div>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent
-              side="right"
-              sideOffset={2}
-              className="mt-1 rounded-md bg-sky-500 px-3 py-1 text-slate-50 dark:bg-neutral-700"
-            >
-              <p>{vote_average.toFixed(1)}</p>
-            </TooltipContent>
-          </Tooltip>
+          <StarRating rating={vote_average} />
         </Link>
       </div>
     </div>
